5d8995f7b2596611233679739d1e025212f02e2d
Caleb Kelly <calebk@Calebs-MacBook-Pro.local>
2020-04-14 13:48:22 -0500

    Initial Commit

---
+++ MainLevel.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var total
+var eggsCollected
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	total = get_tree().get_nodes_in_group("Eggs").size();
+	eggsCollected = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Egg_Count():
+	print("count")
+	eggsCollected += 0
+	$"Player/Camera2D/Game UI/Score".text = str(eggsCollected)
+	pass # Replace with function body.
---
+++ Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const TARGET_FPS = 60
+const ACCELERATION = 25
+const MAX_SPEED = 200
+const FRICTION = 10
+const AIR_RESISTANCE = 15
+const GRAVITY = 20
+const JUMP_FORCE = 400
+
+var motion = Vector2.ZERO
+
+onready var sprite = $Sprite
+onready var animationPlayer = $AnimationPlayer
+
+func _physics_process(delta):
+	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if x_input != 0:
+#		animationPlayer.play("Run")
+		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		sprite.flip_h = x_input < 0
+#	else:
+#		animationPlayer.play("Stand")
+	
+	motion.y += GRAVITY * delta * TARGET_FPS
+	
+	if is_on_floor():
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, FRICTION * delta)
+			
+		if Input.is_action_just_pressed("Jump"):
+			motion.y = -JUMP_FORCE
+#	else:
+#		animationPlayer.play("Jump")
+		
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2
+		
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
+	
+	motion = move_and_slide(motion, Vector2.UP)
